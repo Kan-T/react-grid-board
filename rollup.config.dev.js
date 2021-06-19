a0790db2bd17040367f0b6999b90cb4fdf3aac47
Kan-T <9210063@qq.com>
2021-06-19 11:12:57 +0800

    postcss worked to load css

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -7,14 +7,17 @@ import eslint from "@rollup/plugin-eslint";
 import serve from "rollup-plugin-serve"; // 本地服务器
 import livereload from "rollup-plugin-livereload"; // 热更新插件
 import globals from "rollup-plugin-node-globals"; // React uses process.env.NODE_ENV. This plugin inserts node globals
+import postcss from "rollup-plugin-postcss";
+// import sass from "node-sass";
 
 export default {
   input: "src/sample/index.tsx",
   output: {
-    file: "build/js/sample.bundle.js",
+    file: "public/build/js/sample.bundle.js",
     format: "cjs"
   },
   plugins: [
+    postcss(),
     json(),
     nodeResolve(), //for importing from node_modules
     commonjs(), //Turn CommonJS modules to ES2015 modules
@@ -27,7 +30,7 @@ export default {
     livereload(),
     serve({
       open: true,
-      contentBase: "build",
+      contentBase: ["public"],
       host: "localhost",
       port: 10001
     })
